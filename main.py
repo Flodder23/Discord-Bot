93d6d1a4ede8f0eaa50b8090987745ed622c6c8f
Joe Gibson <joe.gibby@hotmail.co.uk>
2017-12-26 12:54:38 +0000

    sorted into cogs
    this gives them categories in help menu

--- main.py
+++ main.py
@@ -12,58 +12,59 @@ bot = commands.Bot(description = "Very very helpful bot. For code visit https://
 async def on_ready():
     print("Ready")
 
-@bot.command()
-async def ping():
-    """Checks the bot is working.
-    Should reply with "Pong!\"."""
-    await bot.say("Pong!")
+class Games:
+    @commands.command()
+    async def rps(self):
+        """Replies with Rock, Paper or Scissors.
+        (You are expected to claim your action after the \"rps\")"""
+        await bot.say(random.choice(("Rock", "Paper", "Scissors")))
+    @commands.command()
+    async def ping(self):
+        """Checks the bot is working.
+        Should reply with "Pong!\"."""
+        await bot.say("Pong!")
 
-@bot.command()
-async def christmas():
-    """Tells you how long until Christmas.
-    As requested by Adam."""
-    today = datetime.datetime.today()
-    if today.month == 12 and today.day == 25:
-        until = "CHRISTMAS IS TODAY YAY"
-    else:
-        nextXMasYear = today.year
-        if today.month == 12 and today.day >= 25:
-            nextXMasYear += 1
-        until = "Christmas is in "
-        rd = relativedelta(datetime.date(nextXMasYear,12,25), datetime.datetime.today())
-        for a in ("years","months","days","hours","minutes","seconds"):
-            if rd.__dict__[a] != 0:
-                if until != "Christmas is in ":
-                    until += ", "
-                until += str(rd.__dict__[a]) + " " + a
-    await bot.say(until)
+    @commands.command(pass_context = True)
+    async def spam(self, ctx, *, msg):
+        """Sends some spam.
+        You can specify how much spam with a number after the >spam command"""
+        try:
+            for _ in range(int(msg)):
+                await bot.say("This is spam.")
+        except:
+            for _ in range(10):
+                await bot.say("This is spam.")
 
-@bot.command()
-async def rps():
-    """Replies with Rock, Paper or Scissors.
-    (You are expected to claim your action after the \"rps\")"""
-    await bot.say(random.choice(("Rock", "Paper", "Scissors")))
+class Info:
+    @commands.command()
+    async def christmas(self):
+        """Tells you how long until Christmas.
+        As requested by Adam."""
+        today = datetime.datetime.today()
+        if today.month == 12 and today.day == 25:
+            until = "CHRISTMAS IS TODAY YAY"
+        else:
+            nextXMasYear = today.year
+            if today.month == 12 and today.day >= 25:
+                nextXMasYear += 1
+            until = "Christmas is in "
+            rd = relativedelta(datetime.date(nextXMasYear,12,25), datetime.datetime.today())
+            for a in ("years","months","days","hours","minutes","seconds"):
+                if rd.__dict__[a] != 0:
+                    if until != "Christmas is in ":
+                        until += ", "
+                    until += str(rd.__dict__[a]) + " " + a
+        await bot.say(until)
 
-@bot.command()
-async def calc(*, msg):
-    """Performs the desired calculation.
-    To use pi etc. type \"math.pi\"
-    (Please do not use for evil! Thanks.)
-    (also, I can't do algebra etc. yet.)"""
-    if "math" in msg:
-        import math
-    await bot.say(msg + " = " + str(eval(msg)))
-
-@bot.command(pass_context = True)
-async def spam(ctx, *, msg):
-    """Sends some spam.
-    You can specify how much spam with a number after the >spam command"""
-    try:
-        for _ in range(int(msg)):
-            await bot.say("This is spam.")
-    except:
-        for _ in range(10):
-            await bot.say("This is spam.")
+    @commands.command()
+    async def calc(self, *, msg):
+        """Performs the desired calculation.
+        To use pi etc. type \"math.pi\"
+        (Please do not use for evil! Thanks.)
+        (also, I can't do algebra etc. yet.)"""
+        if "math" in msg:
+            import math
+        await bot.say(msg + " = " + str(eval(msg)))
 
 @bot.event
 async def on_message(msg):
@@ -84,6 +85,9 @@ async def on_message(msg):
                                                  "I'd go for ")) +
                                    random.choice(Members))
     await bot.process_commands(msg)
+
+bot.add_cog(Games())
+bot.add_cog(Info())
 
 if os.getenv('BOT_TOKEN') == None:
     print("Running using locally stored value for token")
